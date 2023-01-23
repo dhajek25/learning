9f6bce40bce72b5f622fcfd2d8770c36a6f7730d
dhajek25 <79058813+dhajek25@users.noreply.github.com>
2023-01-23 12:25:01 +0100

    Update and rename SQL_challenge/test to SQL_challenge/1_sql_challenge/1_sql_challenge.sql

---
+++ SQL_challenge/1_sql_challenge/1_sql_challenge.sql
@@ -0,0 +1,32 @@
+-- https://8weeksqlchallenge.com/case-study-1/
+/* 
+ * Danny's Diner
+ * Case Study #1 Questions
+ *  
+*/
+
+-- 1. What is the total amount each customer spent at the restaurant?
+
+SELECT s.customer_id, SUM(m.price) AS total_price
+FROM sales s
+INNER JOIN menu m
+USING (product_id)
+GROUP BY s.customer_id
+ORDER BY s.customer_id;
+
+"A"	76
+"B"	74
+"C"	36
+
+-- 2. How many days has each customer visited the restaurant?
+
+SELECT s.customer_id, COUNT(DISTINCT s.order_date) AS num_days_visits
+FROM sales s
+INNER JOIN menu m
+USING (product_id)
+GROUP BY s.customer_id
+ORDER BY s.customer_id;
+
+"A"	4
+"B"	6
+"C"	2
