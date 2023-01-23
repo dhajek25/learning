739f3c89921d496c6d8a4e4c09c986cec10e9250
dhajek25 <79058813+dhajek25@users.noreply.github.com>
2023-01-23 14:38:49 +0100

    Update 1_sql_challenge.sql

--- SQL_challenge/1_sql_challenge/1_sql_challenge.sql
+++ SQL_challenge/1_sql_challenge/1_sql_challenge.sql
@@ -33,19 +33,24 @@ ORDER BY s.customer_id;
 
 -- 3. What was the first item from the menu purchased by each customer?
 
-SELECT s.customer_id, s.order_date, m.product_name AS first_meal
+WITH CTE_FIRST_MEAL AS(
+SELECT s.customer_id, s.order_date, m.product_name AS first_meal,
+ROW_NUMBER() OVER (PARTITION BY s.customer_id ORDER BY s.order_date, s.product_id) AS RN
 FROM sales s
 INNER JOIN menu m
 USING (product_id)
 WHERE order_date IN 
 (SELECT min(s.order_date) as min_date
 FROM sales s 
-GROUP BY s.customer_id);
+GROUP BY s.customer_id))
+SELECT customer_id, order_date, first_meal
+FROM CTE_FIRST_MEAL
+WHERE RN =1;
 
 "customer_id"	"order_date"	"first_meal"
-"A"	        "2021-01-01"	  "sushi"
-"A"	        "2021-01-01"	  "curry"
-"B"	        "2021-01-01"	  "curry"
+"A"	          "2021-01-01"	  "sushi"
+"B"	          "2021-01-01"	  "curry"
+"C"	          "2021-01-01"	  "ramen"
 "C"	        "2021-01-01"	  "ramen"
 "C"	        "2021-01-01"	  "ramen"
 
