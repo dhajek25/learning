cbd2edcd49c0ac129fbe29fb7af00e30d2b93a1f
dhajek25 <79058813+dhajek25@users.noreply.github.com>
2023-01-24 10:36:17 +0100

    Update 2_sql_challenge.sql

--- SQL_challenge/2_sql_challenge/2_sql_challenge.sql
+++ SQL_challenge/2_sql_challenge/2_sql_challenge.sql
@@ -1,1 +1,9 @@
--- sfsdf
+-- https://8weeksqlchallenge.com/case-study-2/
+-- Pizza Runner - Case Study 2
+
+-- Data preparation
+-- Dealing with 'null' and ''
+UPDATE runner_orders SET pickup_time = NULLIF(pickup_time, 'null');
+
+-- Change date type
+ALTER TABLE runner_orders ALTER COLUMN pickup_time TYPE timestamp USING pickup_time::timestamp; 
